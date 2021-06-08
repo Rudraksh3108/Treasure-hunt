d8c3a30ad2db594bb19e235b7f8370c8d7bae24c
Rudraksh3108 <71761446+Rudraksh3108@users.noreply.github.com>
2021-06-08 15:22:37 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "TIGER";
+const accessCode2 = "ELEPHANT";
+const accessCode3 = "LION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("greti", 100,50)
+    fill("lightblue")
+    text("Hint: national animal !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("phaeetln", 700,70)
+fill("lightblue")
+text("Hint: big animal !!", 700,170)
+fill("white")
+    textSize(15)
+    text("ilno", 100,250)
+    fill("lightblue")
+    text("hint:has a mane !!", 100,270)
+
+}
